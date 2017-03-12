456035ec1673b9367690e048d00e88efe62a6f01
Vincent Lachenal <vincent.lachenal@gmail.com>
2017-03-12 19:40:48 +0100

    Add GPLv3 LICENCE to TypeScritp files

--- src/app/bridge-detail.ts
+++ src/app/bridge-detail.ts
@@ -1,3 +1,20 @@
+/*
+   This file is part of MyHUEnaise.
+
+    MyHUEnaise is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MyHUEnaise is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MyHUEnaise.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 import { Light } from './light';
 import { Group } from './group';
 import { Schedule } from './schedule';
